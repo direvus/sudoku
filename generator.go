37ce06da74cfa90792fb83ed6f85febd16145f9e
Brendan Jurd <direvus@gmail.com>
2018-05-29 21:41:18 +1000

    Refactor attemptSolution out into two methods.
    The code of attemptSolution was pretty lengthy and not easy to unit test
    effectively.  I decided to crack it open and put the seeding logic into
    its own function and make it a method of Puzzle.
    
    Likewise, AttemptSolution is now a method of Puzzle and is exported.
    This takes an already seeded puzzle and tries to find a solution for it
    with some randomness thrown into the mix.

--- generator.go
+++ generator.go
@@ -5,86 +5,100 @@ import (
 	"time"
 )
 
-// attemptSolution tries to randomly generate a valid sudoku solution.
+// SeedSolution populates random glyphs into a sudoku puzzle.
 //
-// It does this by initially populating a small number of cells with random
-// values, then attempting to solve the puzzle using a combination of logical
-// elimination and brute-force guesswork.
+// It does this by randomly selecting an unknown cell, and then trying random
+// glyphs in that cell.  The first glyph tried that does not cause a
+// contradiction is selected for the cell.
 //
-// Regardless of whether it succeeds or fails, it writes a pointer to the
-// generated Puzzle object to the channel.
-//
-// This function makes no guarantees as to the correctness or completeness of
-// its attempt to generate a solution.  The resulting puzzle may be
-// incompletely solved, invalid, or both.  Callers should be prepared to run
-// attemptSolution multiple times and discard bad results.
-func attemptSolution(ch chan *Puzzle) {
-	var puzzle Puzzle
+// The function continues in this manner until it has successfully populated
+// 'n' cells, or it detects an unresolvable cell.  In either case, it writes
+// the number of cells populated to the channel.
+func (puz *Puzzle) SeedSolution(n int, ch chan int) {
 	var glyphs [Size]byte
-	random := rand.New(rand.NewSource(time.Now().UnixNano()))
 	swapper := func(i, j int) {
 		glyphs[i], glyphs[j] = glyphs[j], glyphs[i]
 	}
-
-	// Populate three randomly selected rows of the puzzle with a shuffled copy
-	// of the glyph set.
 	copy(glyphs[:], Glyphs[:])
-	for i := 0; i < SubSize; i++ {
-		// Find a random row that hasn't been populated
-		r := 0
+	i := 0
+	for ; i < n; i++ {
+		if puz.NumUnknowns() == 0 {
+			ch <- i
+			return
+		}
+		// Select an unknown cell at random
+		random := rand.New(rand.NewSource(time.Now().UnixNano()))
+		var r, c int
 		for {
-			r = random.Intn(Size)
-			if !Known(puzzle[r][0]) {
+			r := random.Intn(Size)
+			c := random.Intn(Size)
+			if !Known(puz[r][c]) {
 				break
 			}
 		}
-
-		// Fill the row with randomly re-ordered glyph values and repeat as
-		// necessary if the solution becomes invalid as a result.  It is
-		// possible to get into an unresolvable situation here, so put an
-		// arbitrary upper limit on the number of attempts.
-		for i := 0; i < 100000; i++ {
-			random.Shuffle(len(glyphs), swapper)
-			copy(puzzle[r][:], glyphs[:])
-			if puzzle.Validate() == nil {
+		// Shuffle the glyphs array and try each glyph in turn
+		random.Shuffle(len(glyphs), swapper)
+		for j := 0; j < len(glyphs); j++ {
+			puz[r][c] = glyphs[j]
+			if puz.Validate() == nil {
 				break
 			}
+			puz[r][c] = Unknown
+		}
+		if !Known(puz[r][c]) {
+			break
 		}
 	}
+	ch <- i
+	return
+}
 
-	// Attempt to solve the puzzle using simple elimination.  If the puzzle
-	// remains unsolved, attempt to solve it using guesswork, starting at a
-	// randomly selected unknown cell.
-	puzzle.SolveEasy()
-	if puzzle.NumUnknowns() == 0 {
+// AttemptSolution tries to randomly generate a valid sudoku solution.
+//
+// It does this by attempting to solve the puzzle using a combination of
+// logical elimination and brute-force guesswork starting from a randomly
+// selected unknown cell.
+//
+// The channel receives true if a solution was found, false otherwise.
+func (puz *Puzzle) AttemptSolution(ch chan bool) {
+	// Try simple logical elimination.
+	puz.SolveEasy()
+	if puz.NumUnknowns() == 0 {
+		ch <- true
 		return
 	}
+	// Select a random cell to start guessing from.
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
 	r := random.Intn(Size)
 	c := random.Intn(Size)
 	found := false
-	r, c, found = puzzle.NextUnknown(r, c)
+	r, c, found = puz.NextUnknown(r, c)
 	if !found && (r != 0 || c != 0) {
-		r, c, found = puzzle.NextUnknown(0, 0)
+		r, c, found = puz.NextUnknown(0, 0)
 	}
 	guess := make(chan bool)
-	go puzzle.guess(r, c, guess)
-	<-guess
-	ch <- &puzzle
-	return
+	go puz.guess(r, c, guess)
+	ch <- <-guess
 }
 
 // GenerateSolution returns a randomly generated sudoku solution.
 //
-// It does this by repeatedly calling attemptSolution(), and returning the
-// first puzzle which is both complete and valid.
-func GenerateSolution() (puzzle Puzzle) {
-	ch := make(chan *Puzzle)
+// It does this by repeatedly seeding an empty puzzle with a set of randomly
+// chosen and located glyphs, and then calling AttemptSolution() on the result.
+// It returns the first such puzzle which is both complete and valid.
+func GenerateSolution() (puz Puzzle) {
+	success := make(chan bool)
+	count := make(chan int)
+	n := 27
 	for {
-		go attemptSolution(ch)
-		puzzle = *<-ch
-		if puzzle.Validate() == nil && puzzle.NumUnknowns() == 0 {
-			break
+		go puz.SeedSolution(n, count)
+		if <-count == n {
+			go puz.AttemptSolution(success)
+			if <-success {
+				break
+			}
 		}
+		puz.Clear()
 	}
 	return
 }
--- generator_test.go
+++ generator_test.go
@@ -2,6 +2,110 @@ package sudoku
 
 import "testing"
 
+func TestSeedSolution(t *testing.T) {
+	var puz Puzzle
+	ch := make(chan int)
+	n := 1
+	go puz.SeedSolution(n, ch)
+	result := <-ch
+	if result != n {
+		t.Errorf("incorrect count from SeedSolution(): expected %v cells populated, got %v", n, result)
+	}
+
+	err := puz.Validate()
+	if err != nil {
+		t.Errorf("invalid result from SeedSolution(): %v\n\n%v", err, puz.String())
+	}
+
+	puz.Clear()
+	n = 27
+	go puz.SeedSolution(n, ch)
+	result = <-ch
+	if result > n || result < 1 {
+		t.Errorf("unexpected count from SeedSolution(): expected at least 1 and up to %v cells populated, got %v", n, result)
+	}
+}
+
+func TestAttemptSolution(t *testing.T) {
+	ch := make(chan bool)
+	// “Easy” difficulty
+	puz := Puzzle{
+		{'2', ' ', ' ', '6', '3', ' ', ' ', '1', ' '},
+		{' ', '5', '1', ' ', '2', ' ', '7', '9', '3'},
+		{'4', ' ', '3', '1', '9', '7', '5', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', '9', ' ', '3', '2'},
+		{' ', '6', '5', ' ', '7', ' ', '1', '4', ' '},
+		{'1', '3', ' ', '8', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', '9', '3', '6', '2', '4', ' ', '7'},
+		{'3', '7', '6', ' ', '8', ' ', '2', '5', ' '},
+		{' ', '2', ' ', ' ', '5', '1', ' ', ' ', '9'}}
+	expect := Puzzle{
+		{'2', '9', '7', '6', '3', '5', '8', '1', '4'},
+		{'6', '5', '1', '4', '2', '8', '7', '9', '3'},
+		{'4', '8', '3', '1', '9', '7', '5', '2', '6'},
+		{'7', '4', '8', '5', '1', '9', '6', '3', '2'},
+		{'9', '6', '5', '2', '7', '3', '1', '4', '8'},
+		{'1', '3', '2', '8', '4', '6', '9', '7', '5'},
+		{'5', '1', '9', '3', '6', '2', '4', '8', '7'},
+		{'3', '7', '6', '9', '8', '4', '2', '5', '1'},
+		{'8', '2', '4', '7', '5', '1', '3', '6', '9'}}
+	go puz.AttemptSolution(ch)
+	if !<-ch {
+		t.Errorf("AttemptSolution indicated failure:\n\n%v", puz.String())
+	}
+	if !puz.Equal(expect) {
+		t.Errorf("unexpected result from AttemptSolution: expected:\n%v\n\ngot:\n%v", expect.String(), puz.String())
+	}
+
+	// “Tricky” difficulty
+	puz = Puzzle{
+		{' ', ' ', '3', ' ', '5', ' ', '2', ' ', ' '},
+		{'2', ' ', ' ', '7', ' ', '6', ' ', ' ', '9'},
+		{'7', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '4'},
+		{' ', '2', ' ', '8', ' ', '1', ' ', '6', ' '},
+		{' ', ' ', '9', '6', ' ', '2', '4', ' ', ' '},
+		{' ', '4', ' ', '3', ' ', '5', ' ', '2', ' '},
+		{'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '8'},
+		{'3', ' ', ' ', '4', ' ', '8', ' ', ' ', '2'},
+		{' ', ' ', '5', ' ', '1', ' ', '3', ' ', ' '}}
+	go puz.AttemptSolution(ch)
+	if !<-ch {
+		t.Errorf("AttemptSolution indicated failure:\n\n%v", puz.String())
+	}
+	remain := puz.NumUnknowns()
+	if remain != 0 {
+		t.Errorf("too many unknowns after AttemptSolution: expected %v unknowns remaining, got %v", 0, remain)
+	}
+	err := puz.Validate()
+	if err != nil {
+		t.Errorf("invalid puzzle from AttemptSolution: %v\n\n%v", err, puz.String())
+	}
+
+	// “Extreme” difficulty
+	puz = Puzzle{
+		{' ', ' ', '8', ' ', ' ', '6', '2', '5', ' '},
+		{' ', ' ', ' ', ' ', '7', ' ', ' ', '3', ' '},
+		{' ', ' ', ' ', ' ', '1', '2', '9', '8', ' '},
+		{' ', ' ', '5', ' ', ' ', '3', ' ', ' ', ' '},
+		{' ', '2', ' ', '7', ' ', '1', ' ', '6', ' '},
+		{' ', ' ', ' ', '8', ' ', ' ', '1', ' ', ' '},
+		{' ', '3', '6', '2', '8', ' ', ' ', ' ', ' '},
+		{' ', '7', ' ', ' ', '9', ' ', ' ', ' ', ' '},
+		{' ', '8', '2', '1', ' ', ' ', '4', ' ', ' '}}
+	go puz.AttemptSolution(ch)
+	if !<-ch {
+		t.Errorf("AttemptSolution indicated failure:\n\n%v", puz.String())
+	}
+	remain = puz.NumUnknowns()
+	if remain != 0 {
+		t.Errorf("too many unknowns after AttemptSolution: expected %v unknowns remaining, got %v", 0, remain)
+	}
+	err = puz.Validate()
+	if err != nil {
+		t.Errorf("invalid puzzle from AttemptSolution: %v\n\n%v", err, puz.String())
+	}
+}
+
 func TestGenerateSolution(t *testing.T) {
 	puz := GenerateSolution()
 	unknowns := puz.NumUnknowns()
